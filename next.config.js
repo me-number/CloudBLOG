e6ffcb9190bce6209628d0b2e767c7fd501ed7db
Zhang Wentao <13064145510@wo.cn>
2025-12-27 14:32:41 +0000

    up

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@
  * for Docker builds.
  */
 await import('./src/env.js');
-/** @type {import("next").NextConfig} */
+
+// 使用JavaScript注释替代TypeScript类型注释
 const config = {
   env: {},
+  // 可以添加其他Next.js配置选项
 };
 
 export default config;
